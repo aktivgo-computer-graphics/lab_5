45bfcbab0286d35fa98d97cf802e602e74f6ea47
aktivgo <aktivgo01062001@gmail.com>
2022-03-11 13:10:19 +0300

    Complete task 5, 6

---
+++ lab_5/task/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace task
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paintButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // paintButton
+            // 
+            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.paintButton.Location = new System.Drawing.Point(12, 12);
+            this.paintButton.Name = "paintButton";
+            this.paintButton.Size = new System.Drawing.Size(130, 33);
+            this.paintButton.TabIndex = 0;
+            this.paintButton.Text = "Построить";
+            this.paintButton.UseVisualStyleBackColor = true;
+            this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.paintButton);
+            this.Name = "MainForm";
+            this.Text = "Рекурсия";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button paintButton;
+
+        #endregion
+    }
+}
---
+++ lab_5/task/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace task
+{
+    public partial class MainForm : Form
+    {
+        private Graphics Graph;
+        private Pen MyPen;
+
+        private float x0;
+        private float y0;
+        private float angle = (float) -Math.PI / 2;
+        private float angleMer = (float) Math.PI / 4;
+        private float m = (float) 0.8;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Graph = CreateGraphics();
+            Graph.SmoothingMode = SmoothingMode.HighQuality;
+            MyPen = new Pen(Color.Red);
+        }
+        
+        private void paintButton_Click(object sender, EventArgs e)
+        {
+            Graph.Clear(Color.White);
+            x0 = 300;
+            y0 = 450;
+            //PaintHilbertLabyrinths();
+            PaintTree();
+        }
+
+        private void PaintHilbertLabyrinths()
+        {
+            const float length = 14;
+            const int rec = 5;
+            const int i = 1;
+
+            PaintLab(length, rec, i);
+        }
+        
+        private void PaintLab(float length, int rec, int i)
+        {
+            if (rec == 0) return;
+
+            angle += (float)(i * Math.PI / 2);
+            PaintLab(length, rec - 1, -i);
+            PaintStep(angle, length);
+            
+            angle -= (float)(i * Math.PI / 2);
+            PaintLab(length, rec - 1, i);
+            PaintStep(angle, length);
+            
+            PaintLab(length, rec - 1, i);
+            
+            angle -= (float)(i * Math.PI / 2);
+            PaintStep(angle, length);
+            PaintLab(length, rec - 1, -i);
+            
+            angle += (float)(i * Math.PI / 2);
+        }
+
+        private void PaintStep(float angle, float length)
+        {
+            var x0 = this.x0;
+            var y0 = this.y0;
+            
+            this.x0 += (float)(length * Math.Cos(angle));
+            this.y0 += (float)(length * Math.Sin(angle));
+            
+            Graph.DrawLine(MyPen, x0, y0, this.x0, this.y0);
+        }
+
+        private void PaintTree()
+        {
+            PaintBranch(90);
+        }
+
+        private void PaintBranch(float length)
+        {
+            if (length < 30 || length > 100) return;
+            
+            var x0 = this.x0;
+            var y0 = this.y0;
+            
+            this.x0 += (float)(length * Math.Cos(angle));
+            this.y0 += (float)(length * Math.Sin(angle));
+            
+            Graph.DrawLine(MyPen, x0, y0, this.x0, this.y0);
+            
+            angle -= angleMer / 2;
+            PaintBranch(length * m);
+            
+            angle += angleMer;
+            PaintBranch(length * m);
+            
+            angle -= angleMer / 2;
+            PaintBranch(length * m);
+            
+            this.x0 -= (float)(length * Math.Cos(angle));
+            this.y0 -= (float)(length * Math.Sin(angle));
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Graph.Dispose();
+            MyPen.Dispose();
+        }
+    }
+}
