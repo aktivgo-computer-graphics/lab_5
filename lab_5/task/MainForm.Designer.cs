dfe159270f223dbe20cfdf43201a81b374d23d67
aktivgo <aktivgo01062001@gmail.com>
2022-03-12 20:21:32 +0300

    Refactoring

--- lab_5/task/MainForm.Designer.cs
+++ lab_5/task/MainForm.Designer.cs
@@ -30,12 +30,13 @@
         private void InitializeComponent()
         {
             this.paintButton = new System.Windows.Forms.Button();
+            this.taskSwitcher = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // paintButton
             // 
-            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.paintButton.Location = new System.Drawing.Point(12, 12);
+            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.paintButton.Location = new System.Drawing.Point(227, 12);
             this.paintButton.Name = "paintButton";
             this.paintButton.Size = new System.Drawing.Size(130, 33);
             this.paintButton.TabIndex = 0;
@@ -43,11 +44,24 @@
             this.paintButton.UseVisualStyleBackColor = true;
             this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
             // 
+            // taskSwitcher
+            // 
+            this.taskSwitcher.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.taskSwitcher.FormattingEnabled = true;
+            this.taskSwitcher.Items.AddRange(new object[] { "Задание 1.1", "Задание 1.2", "Задание 1.3", "Задание 1.4", "Задание 1.5", "Задание 2", "Задание 3.1", "Задание 3.2" });
+            this.taskSwitcher.Location = new System.Drawing.Point(12, 15);
+            this.taskSwitcher.Name = "taskSwitcher";
+            this.taskSwitcher.Size = new System.Drawing.Size(200, 28);
+            this.taskSwitcher.TabIndex = 1;
+            this.taskSwitcher.Text = "Выберите задание";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.taskSwitcher);
             this.Controls.Add(this.paintButton);
             this.Name = "MainForm";
             this.Text = "Рекурсия";
@@ -55,6 +69,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.ComboBox taskSwitcher;
 
         private System.Windows.Forms.Button paintButton;
 
--- lab_5/task/MainForm.cs
+++ lab_5/task/MainForm.cs
@@ -9,29 +9,63 @@ namespace task
     public partial class MainForm : Form
     {
         private readonly Graphics Graph;
-        private readonly Pen MyPen;
+        private readonly Pen MyPenBlack;
+        private readonly Pen MyPenRed;
 
         private float x0;
         private float y0;
-        private float angle = (float) -Math.PI / 2;
-        private const float AngleMer = (float)Math.PI / 4;
-        private const float M = (float)0.8;
+        private float angle;
+        private bool isVert;
+        private float angleMer;
+        private float m;
 
         public MainForm()
         {
             InitializeComponent();
             Graph = CreateGraphics();
             Graph.SmoothingMode = SmoothingMode.HighQuality;
-            MyPen = new Pen(Color.Black);
+            MyPenBlack = new Pen(Color.Black, 2);
+            MyPenRed = new Pen(Color.Red, 2);
         }
         
         private void paintButton_Click(object sender, EventArgs e)
         {
             Graph.Clear(Color.White);
-            x0 = 300;
-            y0 = 450;
-            //PaintHilbertLabyrinths();
-            PaintTree();
+            
+            angle = (float)(-Math.PI / 2);
+            angleMer = (float)(Math.PI / 4);
+            m = (float)0.8;
+            isVert = false;
+
+            switch (taskSwitcher.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    x0 = 1000;
+                    y0 = 600;
+                    PaintTree();
+                    break;
+                case 6:
+                    x0 = 700;
+                    y0 = 600;
+                    PaintHilbertLabyrinths();
+                    break;
+                case 7:
+                    x0 = 700;
+                    y0 = 600;
+                    angle = (float)(Math.PI / 4);
+                    PaintHilbertLabyrinths45();
+                    break;
+            }
         }
 
         private void PaintHilbertLabyrinths()
@@ -43,36 +77,88 @@ namespace task
             PaintLab(length, rec, i);
         }
         
+        private void PaintHilbertLabyrinths45()
+        {
+            const float length = 14;
+            const int rec = 4;
+            const int i = 1;
+
+            PaintLab45(length, rec, i);
+        }
+        
         private void PaintLab(float length, int rec, int i)
         {
             if (rec == 0) return;
 
+            isVert = !isVert;
             angle += (float)(i * Math.PI / 2);
             PaintLab(length, rec - 1, -i);
-            PaintStep(angle, length);
+            PaintStep(angle, length, isVert);
             
+            isVert = !isVert;
             angle -= (float)(i * Math.PI / 2);
             PaintLab(length, rec - 1, i);
-            PaintStep(angle, length);
+            PaintStep(angle, length, isVert);
             
             PaintLab(length, rec - 1, i);
             
+            isVert = !isVert;
             angle -= (float)(i * Math.PI / 2);
-            PaintStep(angle, length);
+            PaintStep(angle, length, isVert);
             PaintLab(length, rec - 1, -i);
             
+            isVert = !isVert;
             angle += (float)(i * Math.PI / 2);
         }
+        
+        private void PaintLab45(float length, int rec, int i)
+        {
+            if (rec == 0) return;
 
-        private void PaintStep(float angle, float length)
+            angle += (float)(i * Math.PI / 4);
+            PaintLab45(length, rec - 1, -i);
+            PaintStep(angle, length, isVert);
+
+            angle += (float)(i * Math.PI / 4);
+            PaintLab45(length, rec - 1, i);
+            PaintStep(angle, length, isVert);
+            
+            angle -= (float)(i * Math.PI / 4);
+            PaintLab45(length, rec - 1, -i);
+            PaintStep(angle, length, isVert);
+
+            angle -= (float)(i * Math.PI / 4);
+            PaintStep(angle, length, isVert);
+            PaintLab45(length, rec - 1, -i);
+            
+            angle += (float)(i * Math.PI / 4);
+            PaintLab45(length, rec - 1, i);
+            PaintStep(angle, length, isVert);
+
+            angle += (float)(i * Math.PI / 4);
+            PaintStep(angle, length, isVert);
+            PaintLab45(length, rec - 1, i);
+            
+            angle -= (float)(i * Math.PI / 4);
+            PaintLab45(length, rec - 1, -i);
+            PaintStep(angle, length, isVert);
+
+            angle -= (float)(i * Math.PI / 4);
+            PaintStep(angle, length, isVert);
+            PaintLab45(length, rec - 1, i);
+
+            angle += (float)(i * Math.PI / 4);
+        }
+
+        private void PaintStep(float angle, float length, bool isVert)
         {
             var x0 = this.x0;
             var y0 = this.y0;
             
             this.x0 += (float)(length * Math.Cos(angle));
             this.y0 += (float)(length * Math.Sin(angle));
-            
-            Graph.DrawLine(MyPen, x0, y0, this.x0, this.y0);
+
+            Graph.DrawLine(isVert ? MyPenRed : MyPenBlack, x0, y0, this.x0, this.y0);
         }
 
         private void PaintTree()
@@ -90,16 +176,16 @@ namespace task
             this.x0 += (float)(length * Math.Cos(angle));
             this.y0 += (float)(length * Math.Sin(angle));
             
-            Graph.DrawLine(MyPen, x0, y0, this.x0, this.y0);
+            Graph.DrawLine(MyPenBlack, x0, y0, this.x0, this.y0);
             
-            angle -= AngleMer / 2;
-            PaintBranch(length * M);
+            angle -= angleMer / 2;
+            PaintBranch(length * m);
             
-            angle += AngleMer;
-            PaintBranch(length * M);
+            angle += angleMer;
+            PaintBranch(length * m);
             
-            angle -= AngleMer / 2;
-            PaintBranch(length * M);
+            angle -= angleMer / 2;
+            PaintBranch(length * m);
             
             this.x0 -= (float)(length * Math.Cos(angle));
             this.y0 -= (float)(length * Math.Sin(angle));
@@ -108,7 +194,7 @@ namespace task
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Graph.Dispose();
-            MyPen.Dispose();
+            MyPenRed.Dispose();
         }
     }
 }
